3d3851963f9b6eca75d3b5b6384273b27a59d6d7
Адель Харисов <AdRKharisov@stud.kpfu.ru>
2021-02-27 16:22:24 +0300

    Задачи

---
+++ Train/Train/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Train
+{
+    class Train
+    {
+        private string nameTrain;
+        private int numberTrain;
+        public int amountWagons;
+        public string distanation;
+        public DateTime arrivalTime;
+        public DateTime departureTime;
+        public TimeSpan timeSpan;
+        public DateTime factArrivalTime;
+
+        // число вагонов, станция назначения, время прибытия
+        // время отправления, расчет времени пути, признак опоздания
+
+        public string Name
+        {
+            get { return nameTrain;  }
+
+            set { nameTrain = value; }
+        }
+         public int Number
+        {
+            get { return numberTrain; }
+
+            set { numberTrain = value; }
+        }
+
+        public Train(int amountWagons, string distanation, DateTime arrivalTime,
+            DateTime departureTime, TimeSpan timeSpan, DateTime factArrivalTime)
+        {
+            this.amountWagons = amountWagons;
+            this.distanation = distanation;
+            this.arrivalTime = arrivalTime;
+            this.departureTime = departureTime;
+            this.timeSpan = timeSpan;
+            this.factArrivalTime = factArrivalTime;
+        }
+
+        public void PrintLate()
+        {
+
+            int result = DateTime.Compare(factArrivalTime, arrivalTime);
+            if (result < 0)
+                Console.WriteLine("Поезд приехал раньше, чем ожидалось");
+            else if (result == 0)
+                Console.WriteLine("Поезд приехал как раз");
+            else
+                Console.WriteLine("Поезд опоздал");
+        }
+            
+        public void PrintInfo()
+        {
+            Console.WriteLine("-------(********)--------");
+            Console.WriteLine();
+            Console.WriteLine("Name: {0}", nameTrain);
+            Console.WriteLine("Number: {0}", numberTrain);
+            Console.WriteLine("Amount Wagons: {0}", amountWagons);
+            Console.WriteLine("Distanation: {0}", distanation);
+            Console.WriteLine("Arrival Time: {0}", arrivalTime);
+            Console.WriteLine("Departure Time: {0}", departureTime);
+            Console.WriteLine("Travel Time: {0}", timeSpan);
+            Console.WriteLine("Fact Arrival Time: {0}", factArrivalTime);
+            Console.WriteLine();
+            Console.WriteLine("-------(********)--------");
+
+        }
+    }
+
+
+}
---
+++ Train/Train/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // число вагонов, станция назначения, время прибытия
+            // время отправления, расчет времени пути, признак опоздания
+
+            Console.Write("Введите кол-во вагонов:");
+            int amountWagons = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите название станции прибытия:");
+            string nameStation = Console.ReadLine();
+
+            Console.WriteLine("Введите дату и время прибытия по расписанию: mm/dd/yy hh:mm:ss");
+            string stringArrivalTime = Console.ReadLine();
+            DateTime arrivalTime = DateTime.Parse(stringArrivalTime);
+
+            Console.WriteLine("Введите дату и время отправления по расписанию: mm/dd/yy hh:mm:ss");
+            string stringDepartureTime = Console.ReadLine();
+            DateTime departureTime = DateTime.Parse(stringDepartureTime);
+
+            TimeSpan timeSpan = arrivalTime - departureTime;
+            Console.WriteLine("Поезд в пути: {0}", timeSpan.Duration());
+        
+            Console.WriteLine("Введите фактические дату и время прибытия: mm/dd/yy hh:mm:ss");
+            String stringFactDepartureTime = Console.ReadLine();
+             DateTime factDepartureTime = DateTime.Parse(stringFactDepartureTime);
+
+
+            Train train = new Train(amountWagons, nameStation, arrivalTime, departureTime,
+                timeSpan, factDepartureTime);
+
+            Console.Write("Введите название поезда:");
+            train.Name = Console.ReadLine();
+            Console.Write("Введите номер поезда:");
+            train.Number = int.Parse(Console.ReadLine());
+
+
+            train.PrintInfo();
+            train.PrintLate();
+
+        }
+    }
+
+}
