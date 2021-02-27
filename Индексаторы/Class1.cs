8e9bce913bd16975fe8d283848e7f5471d79c107
Адель Харисов <AdRKharisov@stud.kpfu.ru>
2021-02-27 16:06:39 +0300

    Индексаторы

---
+++ Индексаторы/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Индексаторы
+{
+    class MyArr
+    {
+        // считать и присвоить значения элементам массива
+        // создать св-во, кооторое вернет резутатом размер массива
+        // добавить индексатор: считать и присвоить значения столбцам массива
+
+        private int[,] arr;
+        private int rows, cols;
+        private int Length;
+
+        public MyArr (int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            arr = new int[this.rows, this.cols];
+            Length = rows * cols;
+        }
+        
+        public int ReturnLength
+        {
+            get { return Length; }
+        }
+
+        public int ReturnCols
+        {
+            get { return cols; }
+        }
+
+        public int ReturnRows
+        {
+            get { return rows; }
+        }
+
+        public int this [int index1, int index2]
+        {
+            get { return arr[index1, index2]; }
+            set { arr[index1, index2] = value; }
+        }
+        
+    }
+}
---
+++ Индексаторы/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Индексаторы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArr arr1 = new MyArr(2, 2);
+
+            Console.WriteLine("Введите значения для массива:");
+            for (int i = 0; i < arr1.ReturnRows; i++)
+            {
+                for (int h = 0; h < arr1.ReturnCols; h++)
+                {
+                    arr1[i, h] = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("---------******---------");
+            for (int i = 0; i < arr1.ReturnRows; i++)
+            {
+                for (int h = 0; h < arr1.ReturnCols; h++)
+                {
+                    Console.Write($"arr[{i},{h}] = {arr1[i, h]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Введите номер столбца ({0} - {1})",1,arr1.ReturnCols);
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine("Значения выбранного столбца:");
+            for ( int i = 0 ; i < arr1.ReturnCols; i++)
+            arr1[i,j-1] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Размер массива:{arr1.ReturnLength}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------******---------");
+            for (int i = 0; i < arr1.ReturnRows; i++)
+            {
+                for (int h = 0; h < arr1.ReturnCols; h++)
+                {
+                    Console.Write($"arr[{i},{h}] = {arr1[i, h]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
