35d16994a7cf08d9a5d19b71bd53f151fb9f9b3d
Адель Харисов <AdRKharisov@stud.kpfu.ru>
2021-04-14 15:39:25 +0300

    list

---
+++ Списки/Списки/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Списки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lstA = new List<int>() { 1, 2, 3, 4, 5 };
+            var lstB = lstA.Select((x, i) => x = lstA.Take(i + 1).Sum()).ToList();
+            Console.WriteLine(string.Join(" ", lstB));
+
+        }
+    }
+}
